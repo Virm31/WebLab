243560213fe884da26003ee8ee570f1172df6b23
Virm31 <max.zaxqazaq@gmail.com>
2022-04-20 22:32:24 +0500

    first commit

---
+++ controllers/mainController.js
@@ -0,0 +1,18 @@
+exports.navmenu = [
+    {
+        name: 'Главная',
+        addr: '/'
+    },
+    {
+        name: 'О нас',
+        addr: '#'
+    },
+    {
+        name: 'Рецепты',
+        addr: '#'
+    },
+    {
+        name: 'Предложить свой рецепт',
+        addr: '/contact'
+    }
+];
